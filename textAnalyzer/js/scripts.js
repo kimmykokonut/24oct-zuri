9f6c6be63d25ca767baa4db898b5fdfd1659f9c2
Zuri Gallegos <Zurisadailopezmartinez@gmail.com>
2023-10-24 10:04:36 -0700

    finish textAnalyzer example
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- textAnalyzer/js/scripts.js
+++ textAnalyzer/js/scripts.js
@@ -1,15 +1,26 @@
 // Business Logic
 
-function wordCounter(text) {
-	if (text.trim().length === 0) {
-		return 0;
-	}
-	let wordCount = 0;
+// function wordCounter(text) {
+// 	if (text.trim().length === 0) {
+// 		return 0;
+// 	}
+// 	let wordCount = 0;
+// 	const textArray = text.split(" ");
+// 	textArray.forEach(function (element) {
+// 		if (!Number(element)) {
+// 			wordCount++;
+// 		}
+// 	});
+// 	return wordCount;
+// }
+
+function numberOfOccurrencesInText(word, text) {
 	const textArray = text.split(" ");
-	textArray.forEach(function (element) {
-		if (!Number(element)) {
-			wordCount++;
+	let wordCount = 0;
+	textArray.forEach(function(element) {
+		if (element.toLowerCase().includes(word.toLowerCase())) {
+			wordCount++
 		}
 	});
-	return wordCount;
-}
+		return wordCount;
+	}
